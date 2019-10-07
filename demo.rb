4750110f5ef14d0a2a4d084bcd2d9a70476823da
Lluis Guardiola <lluisguardiola.h@gmail.com>
2019-10-07 00:51:31 +0000

    Automatically backed up by Learn

---
+++ demo.rb
@@ -0,0 +1,9 @@
+the_beatles = [ "John Lennon", "Paul McCartney", "Ringo Starr", "George Harrison"]
+
+english_bands_by_city = {
+ :liverpool =>   "The Beatles",
+ :manchester =>  "The Smiths",
+ :coventry =>    "Delia Derbyshire and the BBC Radiophonic Band",
+ :london =>      "Ziggy Stardust and the Spiders from Mars"
+}
+
